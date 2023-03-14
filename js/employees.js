fd4db0b2bebe80d03bd1085dddfa30f89e0e9405
irenetjrs <irkapidgorodetska@gmail.com>
2023-03-14 20:35:08 +0200

    task 1

--- js/employees.js
+++ js/employees.js
@@ -1,11 +1,11 @@
 export const employees = 
 [
-{name: 'Mike', department: 'frontend', salary: 21001},
-{name: 'Bob', department: 'backend', salary: 17001},
+{name: 'Mike', department: 'frontend', salary: 2100},
+{name: 'Bob', department: 'backend', salary: 1700},
 {name:'Nikola', department: 'frontend', salary: 1900}, 
 {name: 'Alex', department: 'design', salary: 1500}, 
 {name: 'Anna', department: 'sales', salary: 1800},
-{name: 'Maria', department: 'design', salary: 17001,}, 
+{name: 'Maria', department: 'design', salary: 1700,}, 
 {name: 'Jack ', department: 'sales', salary: 1300},
 {name: 'Oliver', department: 'frontend', salary: 1500}, 
 {name: 'Harry', department: 'sales', salary: 1800},
